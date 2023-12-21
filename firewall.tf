4abec0b8daa3c1fa0dccc8212faa39797aaf09b3
Kavipriyan Jeevanadam <www.kavippriyan.j@gmail.com>
2023-12-21 12:09:31 +0530

    changed route for igw and fw by changing vpc_endpoint_id

--- firewall.tf
+++ firewall.tf
@@ -1,77 +1,77 @@
 
-# resource "aws_networkfirewall_rule_group" "SIG_rule_group_1" {
-#   capacity = 50
-#   name     = "SIG-firewall-rule-group-domain-deny"
-#   type     = "STATEFUL"
-#   rule_group {
-#     rules_source {
-#       rules_source_list {
-#         generated_rules_type = "DENYLIST"
-#         target_types         = ["HTTP_HOST"]
-#         targets              = [".google.com"]
-#       }
-#     }
-#     stateful_rule_options {
-#       rule_order = "STRICT_ORDER"
-#     }
-#   }
-# }
-# resource "aws_networkfirewall_rule_group" "SIG_rule_group_2" {
-#   capacity = 10
-#   name     = "deny-ssh"
-#   type     = "STATEFUL"
-#   rule_group {
-#     rules_source {
-#       stateful_rule {
-#         action = "PASS"
-#         header {
-#           destination      = "ANY"
-#           destination_port = 22
-#           direction        = "ANY"
-#           protocol         = "SSH"
-#           source           = "ANY"
-#           source_port      = "ANY"
-#         }
-#         rule_option {
-#           keyword  = "sid"
-#           settings = ["1"]
-#         }
-#       }
-#     }
-#     stateful_rule_options {
-#       rule_order = "STRICT_ORDER"
-#     }
-#   }
-# }
+resource "aws_networkfirewall_rule_group" "SIG_rule_group_1" {
+  capacity = 50
+  name     = "SIG-firewall-rule-group-domain-deny"
+  type     = "STATEFUL"
+  rule_group {
+    rules_source {
+      rules_source_list {
+        generated_rules_type = "DENYLIST"
+        target_types         = ["HTTP_HOST"]
+        targets              = [".google.com"]
+      }
+    }
+    stateful_rule_options {
+      rule_order = "STRICT_ORDER"
+    }
+  }
+}
+resource "aws_networkfirewall_rule_group" "SIG_rule_group_2" {
+  capacity = 10
+  name     = "deny-ssh"
+  type     = "STATEFUL"
+  rule_group {
+    rules_source {
+      stateful_rule {
+        action = "PASS"
+        header {
+          destination      = "ANY"
+          destination_port = 22
+          direction        = "ANY"
+          protocol         = "SSH"
+          source           = "ANY"
+          source_port      = "ANY"
+        }
+        rule_option {
+          keyword  = "sid"
+          settings = ["1"]
+        }
+      }
+    }
+    stateful_rule_options {
+      rule_order = "STRICT_ORDER"
+    }
+  }
+}
 
-# resource "aws_networkfirewall_firewall_policy" "SIG_firewall_policy" {
-#   name = "SIG-firewall-policy"
+resource "aws_networkfirewall_firewall_policy" "SIG_firewall_policy" {
+  name = "SIG-firewall-policy"
 
-#   firewall_policy {
-#     stateless_default_actions          = ["aws:forward_to_sfe"]
-#     stateless_fragment_default_actions = ["aws:forward_to_sfe"]
-#     stateful_rule_group_reference {
-#       priority = 10
-#       resource_arn = aws_networkfirewall_rule_group.SIG_rule_group_2.arn
-#     }
-#     stateful_rule_group_reference {
-#       priority = 1
-#       resource_arn = aws_networkfirewall_rule_group.SIG_rule_group_1.arn
-#     }
-#     stateful_engine_options {
-#       rule_order = "STRICT_ORDER"
-#     }
+  firewall_policy {
+    stateless_default_actions          = ["aws:forward_to_sfe"]
+    stateless_fragment_default_actions = ["aws:forward_to_sfe"]
+    stateful_rule_group_reference {
+      priority = 10
+      resource_arn = aws_networkfirewall_rule_group.SIG_rule_group_2.arn
+    }
+    stateful_rule_group_reference {
+      priority = 1
+      resource_arn = aws_networkfirewall_rule_group.SIG_rule_group_1.arn
+    }
+    stateful_engine_options {
+      rule_order = "STRICT_ORDER"
+    }
    
-#   }
+  }
 
-# }
+}
  
-# resource "aws_networkfirewall_firewall" "SIG_firewall" {
-#   name                = "SIG-firewall"
-#   firewall_policy_arn = aws_networkfirewall_firewall_policy.SIG_firewall_policy.arn
-#   vpc_id              = aws_vpc.SIG_vpc.id
-#   subnet_mapping {
-#     subnet_id = aws_subnet.SIG_firewall_subnet.id
-#   }
-# }
+resource "aws_networkfirewall_firewall" "SIG_firewall" {
+  name                = "SIG-firewall"
+  firewall_policy_arn = aws_networkfirewall_firewall_policy.SIG_firewall_policy.arn
+  vpc_id              = aws_vpc.SIG_vpc.id
+  subnet_mapping {
+    subnet_id = aws_subnet.SIG_firewall_subnet.id
+  }
+}
 
--- vpc.tf
+++ vpc.tf
@@ -46,12 +46,12 @@ resource "aws_route_table_association" "SIG_subnet_association" {
   route_table_id = aws_route_table.SIG_subnet_rt.id
 }
 
-#  resource "aws_route" "SIG_subnet_to_firewall" {
-#    route_table_id         = aws_route_table.SIG_subnet_rt.id
-#    destination_cidr_block = "0.0.0.0/0"
-#    vpc_endpoint_id        = aws_networkfirewall_firewall.SIG_firewall.firewall_status[0].sync_states[0].attachment[0].endpoint_id
-#    depends_on = [aws_networkfirewall_firewall.SIG_firewall]
-#  }
+ resource "aws_route" "SIG_subnet_to_firewall" {
+   route_table_id         = aws_route_table.SIG_subnet_rt.id
+   destination_cidr_block = "0.0.0.0/0"
+   vpc_endpoint_id        = element([for ss in tolist(aws_networkfirewall_firewall.SIG_firewall.firewall_status[0].sync_states) : ss.attachment[0].endpoint_id if ss.attachment[0].subnet_id == aws_subnet.SIG_subnet.id],0)
+ depends_on = [aws_networkfirewall_firewall.SIG_firewall]
+ }
 
 
 resource "aws_route_table" "SIG_igw_rt" {
@@ -62,12 +62,12 @@ resource "aws_route_table_association" "SIG_igw_rt_association" {
   route_table_id = aws_route_table.SIG_igw_rt.id
 }
 
-# resource "aws_route" "SIG_igw_to_firewall" {
-#   route_table_id         = aws_route_table.SIG_igw_rt.id
-#   destination_cidr_block = "172.17.42.0/27"
-#  vpc_endpoint_id        = aws_networkfirewall_firewall.SIG_firewall.firewall_status[0].sync_states[0].attachment[0].endpoint_id
-#  depends_on = [aws_networkfirewall_firewall.SIG_firewall]
-# }
+resource "aws_route" "SIG_igw_to_firewall" {
+  route_table_id         = aws_route_table.SIG_igw_rt.id
+  destination_cidr_block = "172.17.42.0/27"
+ vpc_endpoint_id        = element([for ss in tolist(aws_networkfirewall_firewall.SIG_firewall.firewall_status[0].sync_states) : ss.attachment[0].endpoint_id if ss.attachment[0].subnet_id == aws_subnet.SIG_subnet.id],0)
+ depends_on = [aws_networkfirewall_firewall.SIG_firewall]
+}
 
 
 resource "aws_route_table" "SIG_firewall_rt" {
