f19bdac1e6288e9f5f77417ea1ebd937646af80f
Kavipriyan Jeevanadam <www.kavippriyan.j@gmail.com>
2023-12-22 18:45:34 +0530

    changed ec2 instance emi

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 
 # Create EC2 instances in the hub and spoke subnets
 resource "aws_instance" "hub_ec2_instance" {
-  ami             = "ami-0645cf88151eb2007" # RedHat AMI
+  ami             = "ami-023c11a32b0207432" # RedHat AMI
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.SIG_subnet.id
   key_name        = "oskeypair"
