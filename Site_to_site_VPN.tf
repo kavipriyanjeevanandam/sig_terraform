5e1feff7781b2cb27338d3a0066a3a13a9c401ec
Kavipriyan Jeevanadam <www.kavippriyan.j@gmail.com>
2023-12-22 18:20:48 +0530

    updated site to site vpn ip prefix details

--- Site_to_site_VPN.tf
+++ Site_to_site_VPN.tf
@@ -23,15 +23,15 @@ resource "aws_customer_gateway" "other_vpc_customer_gateway" {
 }
 
 # Create a VPN Connection
-resource "aws_vpn_connection" "hub_to_other_vpc_vpn_connection" {
+resource "aws_vpn_connection" "SIG_vpc_to_other_vpc_vpn_connection" {
   customer_gateway_id = aws_customer_gateway.other_vpc_customer_gateway.id
   vpn_gateway_id      = aws_vpn_gateway.hub_vpn_gateway.id
   type                = "ipsec.1"
   static_routes_only = true
 }
 resource "aws_vpn_connection_route" "ip_prefix" {
-  destination_cidr_block = "172.17.36.0/22"  # IP Prefix
-  vpn_connection_id      = aws_vpn_connection.hub_to_other_vpc_vpn_connection.id
+  destination_cidr_block = "10.0.0.0/16"  # IP Prefix
+  vpn_connection_id      = aws_vpn_connection.SIG_vpc_to_other_vpc_vpn_connection.id
 }
 
 
