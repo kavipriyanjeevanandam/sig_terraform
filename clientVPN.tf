2314655c630f1c43e9d4bf7b95cbdc2680ae48d0
Kavipriyan Jeevanadam <www.kavippriyan.j@gmail.com>
2023-12-25 19:13:41 +0530

    changed certificate arn

--- clientVPN.tf
+++ clientVPN.tf
@@ -1,11 +1,11 @@
 resource "aws_ec2_client_vpn_endpoint" "my_client_vpn" {
-  server_certificate_arn = "arn:aws:acm:us-east-1:833364529536:certificate/a285ab16-8dc7-4b2c-9fa5-de6be7f7a40c"  # server certificate ARN
+  server_certificate_arn = "arn:aws:acm:us-east-1:833364529536:certificate/f84c652a-730c-4503-8d1f-817f3a1d6caf"  # server certificate ARN
   client_cidr_block      = "10.0.0.0/16"
   vpc_id                 = aws_vpc.SIG_vpc.id
   split_tunnel = true
   authentication_options {
     type          = "certificate-authentication"
-    root_certificate_chain_arn = "arn:aws:acm:us-east-1:833364529536:certificate/a285ab16-8dc7-4b2c-9fa5-de6be7f7a40c"  #  root certificate ARN
+    root_certificate_chain_arn = "arn:aws:acm:us-east-1:833364529536:certificate/f84c652a-730c-4503-8d1f-817f3a1d6caf"  #  root certificate ARN
   }
   connection_log_options {
     enabled       = false
