aee85f6b632aa78faff161748d52d3bb25665cbe
Kavipriyan Jeevanadam <www.kavippriyan.j@gmail.com>
2023-12-25 19:09:39 +0530

    changed key name for ec2

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "hub_ec2_instance" {
   ami             = "ami-023c11a32b0207432" # RedHat AMI
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.SIG_subnet.id
-  key_name        = "oskeypair"
+  key_name        = "sigkeypair"
   security_groups = [aws_security_group.SIG_vpc_sg.id]
   tags = {
     Name = "SIG_EC2"
